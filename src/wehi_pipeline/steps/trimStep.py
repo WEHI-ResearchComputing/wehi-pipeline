3baa85d8a8d916a9cb019cd7b5912b661aae3adf
Evan Thomas <thomas.e@wehi.edu.au>
2017-03-13 16:22:16 +1100

    Better control of java processes, in particular the temp dir and memory

--- src/wehi_pipeline/steps/trimStep.py
+++ src/wehi_pipeline/steps/trimStep.py
@@ -8,12 +8,14 @@ import os
 
 from wehi_pipeline.steps.jobStep import ConfigJobStep
 from wehi_pipeline.toil_support.utils import execute
+from wehi_pipeline.toil_support.shellModules import javaHelper
 from wehi_pipeline.config.symbols import resolveSymbols, findOutputFile
 from wehi_pipeline.config import ConfigException
 
 TRIMMOMATIC = 'trimmomatic'
-NP_THREADS = 8
 ADAPTORS = 'ILLUMINACLIP:/stornext/System/data/apps/trimmomatic/trimmomatic-0.36/adapters/TruSeq3-PE.fa:1:30:20:4:true'
+NP_THREADS = 8
+
 
 class Trim(ConfigJobStep):
 
@@ -44,19 +46,30 @@ class Trim(ConfigJobStep):
             self._trimmer = config['trimmer']
             if self._trimmer != TRIMMOMATIC:
                 raise ConfigException('Only %s is supporter at the moment' % TRIMMOMATIC)
-        else:
-            self._trimmer = TRIMMOMATIC
             
+    def numRecommendedThreads(self):
+        return NP_THREADS
+    
+    def recommendedMemory(self):
+        return 60
+    
     def function(self):
         def f(job, context):
             
-            cmd = 'sh ' + self._trimmer + ' PE -threads ' + str(NP_THREADS) + ' $forward  $backward' 
+            numThreads = str(self.numRecommendedThreads())
+            ram = str(self.recommendedMemory())
+            
+            jar = javaHelper(self.modules(), TRIMMOMATIC)
+            if jar is None:
+                raise Exception('Could not find ' + TRIMMOMATIC + ' jar')
+                        
+            cmd = 'java -Djava.io.tmpdir=$temp-dir ' + '-Xmx' + ram + 'g -jar ' + jar + ' PE -threads ' + numThreads + ' $forward  $backward' 
             (cmd, outputFiles) = resolveSymbols(job, context, cmd, self.symbols())
             
             anOutput = findOutputFile(context, 'trimmed-backward')
             baseOut = os.path.join(os.path.dirname(anOutput.path()), 'trimmed.fastq.gz')
             
-            adaptors = self._getAdaptors()
+            adaptors = self._getAdaptors(jar)
             
             cmd = cmd[0] + ' -baseout ' + baseOut + ' ' + adaptors
             
@@ -66,6 +79,7 @@ class Trim(ConfigJobStep):
                 
         return f
 
-    def _getAdaptors(self):
-        # Apaptors can be worked out using the location of the trimmer software_version
+    def _getAdaptors(self, jar):
+        # Apaptors could be worked out using the location of the trimmer software_version
         return ADAPTORS
+    
--- src/wehi_pipeline/toil_support/shellModules.py
+++ src/wehi_pipeline/toil_support/shellModules.py
@@ -4,6 +4,9 @@ Created on 17Feb.,2017
 @author: thomas.e
 '''
 
+from __future__ import division
+import string 
+
 import os
 import subprocess
 
@@ -92,3 +95,66 @@ def _actionFor(words):
         return _actions[action](words[1:])
     else:
         return None
+
+def javaHelper(modules, cmd):
+#     import pydevd
+#     pydevd.settrace('10.1.17.53', stdoutToServer=True, stderrToServer=True, suspend=False)
+    
+    env = addModules(modules)
+    
+    fn = _which(cmd, env)
+    
+    if fn is None:
+        return None
+    
+    with open(fn) as fh:
+        for line in fh:
+            words = line.split()
+            if words is None:
+                continue
+            l = len(words)
+            indx = words.index('-jar')
+            if indx > -1 and l-1 > indx:
+                return words[indx+1]
+            
+    return None
+ 
+def _which(program, env):
+    # http://stackoverflow.com/questions/377017/test-if-executable-exists-in-python
+    
+    def is_exe(fpath):
+        return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+
+    fpath, _ = os.path.split(program)
+    if fpath:
+        if is_exe(program):
+            return program
+    else:
+        for path in env["PATH"].split(os.pathsep):
+            path = path.strip('"')
+            exe_file = os.path.join(path, program)
+            if is_exe(exe_file):
+                return exe_file
+
+    return None
+
+def _isText(filename):
+    # http://stackoverflow.com/questions/1446549/how-to-identify-binary-and-text-files-using-python
+
+    s=open(filename).read(512) # I assume this will close the file!!
+    text_characters = "".join(map(chr, range(32, 127)) + list("\n\r\t\b"))
+    _null_trans = string.maketrans("", "")
+    if not s:
+        # Empty files are considered text
+        return True
+    if "\0" in s:
+        # Files with null bytes are likely binary
+        return False
+    # Get the non-text characters (maps a character to itself then
+    # use the 'remove' option to get rid of the text characters.)
+    t = s.translate(_null_trans, text_characters)
+    # If more than 30% non-text characters, then
+    # this is considered a binary file
+    if float(len(t))/float(len(s)) > 0.30:
+        return False
+    return True
