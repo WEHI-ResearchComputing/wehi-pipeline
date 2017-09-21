c98eceaadc9d82cdc1ccc42fef929d34458754b1
Evan Thomas <thomas.e@wehi.edu.au>
2017-09-21 10:13:38 +1000

    more attempts to get setup.py correct

--- setup.py
+++ setup.py
@@ -4,12 +4,11 @@ from setuptools import find_packages
 setup(
     name='wehi-pipeline',
     version='0.1',
-    packages=['src.wehi_pipeline', 'src.wehi_pipeline.batchSystems'],
+    packages=['wehi_pipeline', 'src.wehi_pipeline.batchSystems'],
     url='https://github.com/WEHI-ResearchComputing/wehi-pipeline',
     license='GPLv3',
     author='Evan Thomas',
     author_email='thomas.e@wehi.edu.au',
     description='Wrappers and utilities to run CWL/Toil pipelines on WEHI infrastructure',
-    packages=find_packages(where='src'),
     entry_points={'console_scripts': ['cwlwehi = wehi_pipeline.cwlwehi:main']}
 )
